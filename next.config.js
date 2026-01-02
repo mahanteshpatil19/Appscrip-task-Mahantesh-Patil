aeda4f2bd85a33546bc341a5bd291438770678b1
Mahantesh Patil <mahantesh@example.com>
2026-01-02 21:41:55 +0530

    Appscrip PLP assignment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  output: "export",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
