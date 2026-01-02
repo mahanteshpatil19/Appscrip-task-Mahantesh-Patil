c1df94d61e1278fffc5e552541f6a560aa5f41a4
Mahantesh Patil <mahantesh@example.com>
2026-01-02 21:45:34 +0530

    Appscrip  assignment

--- pages/index.js
+++ pages/index.js
@@ -61,9 +61,14 @@ export default function Home({ products }) {
   );
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const res = await fetch("https://fakestoreapi.com/products");
   const products = await res.json();
 
-  return { props: { products } };
+  return {
+    props: {
+      products,
+    },
+  };
 }
+
